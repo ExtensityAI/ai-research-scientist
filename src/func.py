333fba621917c672399ad788f030abd2d6e8da29
leoentersthevoid <c.leov@icloud.com>
2024-02-09 00:32:08 +0200

    isolated the benchmark prototype

--- src/func.py
+++ src/func.py
@@ -1,18 +1,46 @@
-from symai import Expression, Function
+from beartype import beartype
+from symai import Symbol, Expression, Function
+from symai.components import FileReader
+from pathlib import Path
+from components import Paper, RelatedWork, Cite, Abstract, Title, Method, Source
 
 
-FUNCTION_DESCRIPTION = '''
-# TODO: Your function description here.
-{template}
-'''
 
-
-class MyExpression(Expression):
-    def __init__(self, **kwargs):
+class DocumentGenerator(Expression):
+    @beartype
+    def __init__(
+        self,
+        **kwargs
+    ):
         super().__init__(**kwargs)
-        self.fn = Function(FUNCTION_DESCRIPTION)
 
-    def forward(self, data, template: str = '', *args, **kwargs):
-        data = self._to_symbol(data)
-        self.fn.format(template=template)
-        return self.fn(data, *args, **kwargs)
+    @beartype
+    def forward(
+        self,
+        task: Symbol,
+        hierarchy: Expression,
+    ) -> Symbol:
+        document = hierarchy(task)
+
+        return document
+
+if __name__ == "__main__":
+    dir_path = Path(__file__).parent.absolute() / "documents"
+    task = Symbol("[Objective]\nWrite a paper about the SymbolicAI framework. Include citations and references from the referenced papers. Follow primarily the [Task] instructions.")
+    hierarchy = Paper(
+        Method(
+            Source(file_link=(dir_path / "method/symbolicai_docs.txt").as_posix()),
+        ),
+        RelatedWork(
+            Cite(file_link=(dir_path / "bib/related_work/newell56.txt").as_posix()),
+            Cite(file_link=(dir_path / "bib/related_work/newell57.txt").as_posix()),
+            Cite(file_link=(dir_path / "bib/related_work/laird87.txt").as_posix()),
+            Cite(file_link=(dir_path / "bib/related_work/newell72.txt").as_posix()),
+            Cite(file_link=(dir_path / "bib/related_work/mccarthy06.txt").as_posix()),
+        ),
+        Abstract(),
+        Title(),
+    )
+
+    doc_gen = DocumentGenerator()
+    res = doc_gen(task, hierarchy)
